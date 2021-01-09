405027c47c3c7944357c1022fa4744876e1addd7
syzygyzer <jon@tlon.io>
2021-01-09 21:04:49 +0000

    working dumb form

--- rpi/processing.py
+++ rpi/processing.py
@@ -42,11 +42,17 @@ arg_list = ['one', 'two', 'three', 'four', 'five']
 
 
 class BigDumbForm(FlaskForm):
-    layer1 = FormField(DLayerForm(0))
-    layer2 = FormField(DLayerForm(1))
-    layer3 = FormField(DLayerForm(2))
-    layer4 = FormField(DLayerForm(3))
-    layer5 = FormField(DlayerForm(4))
+    layerText1 = StringField('layer 1 text')
+    layerText2 = StringField('layer 2 text')
+    layerText3 = StringField('layer 3 text')
+    layerText4 = StringField('layer 4 text')
+    layerText5 = StringField('layer 5 text')
+    layerSpeed1 = StringField('layer 1 speed')
+    layerSpeed2 = StringField('layer 2 speed')
+    layerSpeed3 = StringField('layer 3 speed')
+    layerSpeed4 = StringField('layer 4 speed')
+    layerSpeed5 = StringField('layer 5 speed')
+
     submit = SubmitField('submit')
 
 
@@ -83,18 +89,28 @@ def index():
     return render_template('layerform.html', title='Layers', form=forms[0])
 
 @app.route('/dumb', methods=['GET', 'POST'])
-def index():
+def dumb():
     form = BigDumbForm()
     currentText = ["" for i in range(5)]
-    currentSpeed = ["" for i in range(5)]
-    for i in range(5):
-        if form.validate_on_submit():
-            currentText[i] = str(forms.layerText.data)
-            currentSpeed[i] = str(forms.layerSpeed.data)
+    currentSpeed = [50 for i in range(5)]
+    if form.validate_on_submit():
+        currentText[0] = str(form.layerText1.data)
+        currentText[1] = str(form.layerText2.data)
+        currentText[2] = str(form.layerText3.data)
+        currentText[3] = str(form.layerText4.data)
+        currentText[4] = str(form.layerText5.data)
+        currentSpeed[0] = str(form.layerSpeed1.data)
+        currentSpeed[1] = str(form.layerSpeed2.data)
+        currentSpeed[2] = str(form.layerSpeed3.data)
+        currentSpeed[3] = str(form.layerSpeed4.data)
+        currentSpeed[4] = str(form.layerSpeed5.data)
     for i in range(5):
         if currentText[i] is not None and currentText[i] is not "":
             layerStart[i+1](currentText[i].encode('utf-8'), -1)
-    return render_template('layerform.html', title='Layers', form=forms[0])
+        if currentSpeed[i] is not None and currentSpeed[i] is not "":
+            layerSpeed[i+1](int(currentSpeed[i]))
+
+    return render_template('dumbform.html', title='Layers', form=form)
 
 
 @app.route("/layer1")
