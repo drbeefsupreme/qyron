7c4f9367a850d60cee6e5d3743b641be9f4a67bd
syzygyzer <jon@tlon.io>
2021-01-09 01:01:47 +0000

    starting over real

--- rpi/main.py
+++ rpi/main.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 
-#reads serial messages
-import serial #this is actually the pyserial package, installed globally cuz there didnt seem to be another way
+#RPC library
+from simple_rpc import Interface
 import time
 
-#other project files
-#from .Connector import Connector
-from connector import Connector
-from controller import Controller
-from helperFunctions import HelperFunctions
-
-#REPL
-#from code import InteractiveConsole
-#header = "THE QUANTUM METACHYRON"
-#footer = "YOU THINK REALITY IS REAL?"
 
 VERBOSE = True
 SERIAL_PORT = '/dev/ttyACM0'
@@ -25,47 +15,19 @@ if __name__ == '__main__':
         'read_timeout': READ_TIMEOUT,
         'verbose': VERBOSE
     }
-    HelperFunctions(options=options)
 
-    connector = Connector(serial_port=SERIAL_PORT, read_timeout=READ_TIMEOUT, verbose=VERBOSE)
-    connector.connect_to_serial_port()
-    conn = connector.get_serial_connection()
+    interface = Interface(SERIAL_PORT)
 
-
-
-    if conn is None:
+    if interface is None:
         raise Exception('[-] connect_to_serial_port failed')
 
-    controller = Controller(conn=conn, verbose=VERBOSE)
 
-    print("name, open, waiting:")
-    print(conn.name)
-    print(conn.is_open)
-    print(conn.in_waiting)
+    print("connection successful")
 
     time.sleep(3)
-    controller.display_text("advancedness")
-    time.sleep(3)
-    controller.test_layer_1()
 
-"""
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=5)
-    ser.flush()
-    print("test1")
-    print(ser.name)
-    print(ser.is_open)
-    print(ser.in_waiting)
-
-    layer1command = b'\xc9\xc0\x00\x01\x01\x36'
-    layer2command = b'\xc9\xc0\x00\x01\x02\x36'
-    stringCommand = b'\xc9\xc0\x00\x03' + b' bweh'
-    stringCommand2 = b'\xc9\xc0\x00\x03' + b' skrrt'
-    #stringText = b'hello'
-    #stringPacket = stringCommand + stringText
-
-    time.sleep(2)
-    ser.write(layer1command)
-    time.sleep(2)
-    ser.write(layer2command)
-"""
+    interface.scrollingLayer1_start(b'test', -1)
+    time.sleep(1)
+    interface.scrollingLayer2_start(b'bweh skrrters', -1)
+    time.sleep(1)
+    interface.scrollingLayer1_speed(10)
