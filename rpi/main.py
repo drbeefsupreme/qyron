2fe3d8c7adfb97de81a3464477568f1842647475
syzygyzer <jon@tlon.io>
2021-01-09 02:03:20 +0000

    working forms

--- rpi/main.py
+++ rpi/main.py
@@ -3,9 +3,11 @@
 #RPC library
 from simple_rpc import Interface
 #web app library
-from flask import Flask
+from flask import Flask, request
 import time
 
+#my libraries
+from processing import do_calculation
 
 VERBOSE = True
 SERIAL_PORT = '/dev/ttyACM0'
@@ -13,9 +15,43 @@ READ_TIMEOUT = 5
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "she a go"
+    errors = ""
+    if request.method == "POST":
+        number1 = None
+        number2 = None
+        try:
+            number1 = float(request.form["number1"])
+        except:
+            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number1"])
+        try:
+            number2 = float(request.form["number2"])
+        except:
+            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number2"])
+        if number1 is not None and number2 is not None:
+            result = do_calculation(number1, number2)
+            return '''
+                <html>
+                    <body>
+                        <p>The result is {result}</p>
+                        <p><a href="/">Click here to calculate again</a>
+                    </body>
+                </html>
+            '''.format(result=result)
+    return '''
+        <html>
+            <body>
+                {errors}
+                <p>Enter your numbers:</p>
+                <form method="post" action=".">
+                    <p><input name="number1" /></p>
+                    <p><input name="number2" /></p>
+                    <p><input type="submit" value="Do calculation" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(errors=errors)
 
 @app.route("/layer1")
 def layer1text():
