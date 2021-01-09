44b1b44cc595e8bd94f4965d9da115a9df4c3c1a
syzygyzer <jon@tlon.io>
2021-01-09 02:30:15 +0000

    separate processing

--- rpi/main.py
+++ rpi/main.py
@@ -6,60 +6,14 @@ from simple_rpc import Interface
 from flask import Flask, request
 import time
 
-#my libraries
-from processing import do_calculation
-
 VERBOSE = True
 SERIAL_PORT = '/dev/ttyACM0'
 READ_TIMEOUT = 5
 
 app = Flask(__name__)
+interface = Interface(SERIAL_PORT)
+import processing
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    errors = ""
-    if request.method == "POST":
-        number1 = None
-        number2 = None
-        try:
-            number1 = float(request.form["number1"])
-        except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number1"])
-        try:
-            number2 = float(request.form["number2"])
-        except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number2"])
-        if number1 is not None and number2 is not None:
-            result = do_calculation(number1, number2)
-            return '''
-                <html>
-                    <body>
-                        <p>The result is {result}</p>
-                        <p><a href="/">Click here to calculate again</a>
-                    </body>
-                </html>
-            '''.format(result=result)
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Enter your numbers:</p>
-                <form method="post" action=".">
-                    <p><input name="number1" /></p>
-                    <p><input name="number2" /></p>
-                    <p><input type="submit" value="Do calculation" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
-
-@app.route("/layer1")
-def layer1text():
-    interface.scrollingLayer1_start(b'Layer 1 test', -1)
-
-@app.route("/layer2")
-def layer2text():
-    interface.scrollingLayer2_start(b'layer 2 test', -1)
 
 if __name__ == '__main__':
     options = {
@@ -68,7 +22,6 @@ if __name__ == '__main__':
         'verbose': VERBOSE
     }
 
-    interface = Interface(SERIAL_PORT)
 
     if interface is None:
         raise Exception('[-] connect_to_serial_port failed')
--- rpi/processing.py
+++ rpi/processing.py
@@ -1,2 +1,44 @@
-def do_calculation(number1, number2):
-    return number1 + number2
+from __main__ import app, interface
+from flask import request
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    errors = ""
+    if request.method == "POST":
+        layer1 = None
+        layer2 = None
+        try:
+            layer1 = str(request.form["layer1"])
+        except:
+            errors += "<p>{!r} is not a string.</p>\n".format(request.form["layer1"])
+        try:
+            layer2 = str(request.form["layer2"])
+        except:
+            errors += "<p>{!r} is not a string.</p>\n".format(request.form["layer2"])
+        if layer1 is not None and layer1 is not "":
+            interface.scrollingLayer1_start(layer1.encode('utf-8'), -1)
+        if layer2 is not None and layer2 is not "":
+            interface.scrollingLayer2_start(layer2.encode('utf-8'), -1)
+
+    return '''
+        <html>
+            <body>
+                {errors}
+                <p>Enter your layers:</p>
+                <form method="post" action=".">
+                    <p><input name="layer1" /></p>
+                    <p><input name="layer2" /></p>
+                    <p><input type="submit" value="Set layers" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(errors=errors)
+
+
+@app.route("/layer1")
+def layer1text():
+    interface.scrollingLayer1_start(b'Layer 1 test', -1)
+
+@app.route("/layer2")
+def layer2text():
+    interface.scrollingLayer2_start(b'layer 2 test', -1)
